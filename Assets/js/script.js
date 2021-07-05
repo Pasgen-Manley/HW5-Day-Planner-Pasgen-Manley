70c5a0e76d39b377e05cdc628ba712aa48c636f9
Pasgen Manley <pasgen95@gmail.com>
2021-07-05 21:05:16 +0800

    updated README file

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,12 +6,15 @@ $("#currentDay").text(currentDay);
 //Varibles
 var currentTime = moment();
 var plannerContainer = $(".container");
-var hourArray = []
+var hourArray = [];
 
-
-// Functions callbacks
+// Function callbacks
 createHourArray();
 createTimeBlocks();
+renderEvents();
+
+
+
 
 // Create Hour Array
 function createHourArray() {
@@ -19,9 +22,23 @@ function createHourArray() {
         var hour = moment().hour(i + 9).format("hA")
         hourArray.push(hour);
         console.log(hourArray);
-    }
+    };
     
-}
+};
+
+
+//$('button').each(function () {
+
+
+function renderEvents() {
+    var displayEvent = JSON.parse(localStorage.getItem("events"));
+    $("textarea").empty();
+    $.each(hourArray, function (i, value){
+        $("#text" + i).text(displayEvent[i]);
+       
+    });
+    //console.log(displayEvent);
+};
 // Start by creating for loop which creates day planner table.
 
 function createTimeBlocks() {
@@ -34,24 +51,37 @@ function createTimeBlocks() {
         timeBlockRow.addClass('row time-block');
         hourCol.addClass('hour col-2').attr('data-hour');
         hourCol.text(hourArray[i]);
-        eventCol.addClass('description col-8');
-        saveCol.addClass('saveBtn col-2').html("<i class=\"far fa-save\"></i>");
+        eventCol.addClass('description col-8').attr("id", "text" + i).attr("data-row", i);
+        saveCol.addClass('saveBtn col-2').attr("id", "buttonNumber" + i).attr("data-row", i).html("<i class=\"far fa-save\"></i>");
 
         plannerContainer.append(timeBlockRow);
         timeBlockRow.append(hourCol, eventCol, saveCol);
 
+       
         // Change colors of timeblocks depending on the time of day
-        //if current time of day timeblock displays red
+        // If current time of day timeblock displays red
         if (currentTime.isSame(moment('9:00 AM', 'hh:mm A').add(i, 'hours'), 'hour')) {
             $(eventCol).addClass('present');
-            //if in the future, display green
+            // if in the future, display green
         } else if (currentTime.isBefore(moment('9:00 AM', 'hh:mm A').add(i, 'hours'), 'hour')) {
             $(eventCol).addClass('future');
             // If in the past display grey
         } else if (currentTime.isAfter(moment('9:00 AM', 'hh:mm A').add(i, 'hours'), 'hour')) {
             $(eventCol).addClass('past');
-        }
-    }
+        };
+    };
     
+    
+    $('.saveBtn').on('click', function() {
+        //console.log(hourArray);
+        var eventInput = $(this).siblings('.description').val();
+        //console.log($(this));
+
+        localStorage.setItem("events", JSON.stringify(eventInput));
+        //console.log(localStorage);
+    });
+
 };
- 
+
+// Saving 
+// map function. transform an array to empty text values.
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # HW5-Day-Planner-Pasgen-Manley
+
+## Link to application
+
+
+### Project Description
+The project was to develop a day planner application. This application was to have the standard office hours (9-5) separated into columns for each hour. In each column, the user could input text which they can save upon refresh. The time blocks color is reactive to the time of day. If it is the current hour the time block appears as red, if it is in the past it appears as grey and finally, the future time blocks are green.
+
+#### Application screen example
+
+How the day planner appears when first opening the application.
+![Day planner initial look.](./Assets/images/day-planner.png)
+
+
+How the application looks when events are added and saved.
+![Saved event items for reminders.](./Assets/images/filled-in-planner.png)
